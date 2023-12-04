0d078121d736e363412ee8644671524ead192317
Anand Raj <anandraj.solutions@gmail.com>
2023-12-04 22:02:55 +0530

    project at final stage without debugging

--- BlogHub/src/components/index.js
+++ BlogHub/src/components/index.js
@@ -4,5 +4,10 @@ import Container from "./container/Container";
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-
-export {Header, Footer, Container, Logo, LogoutBtn, RTE}
+import Signup from "./Signup";
+import Login from "./Login";
+import Post from "./Post";
+import PostForm from "./Postform";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+export {Header, Footer, Container, Logo, LogoutBtn, RTE, Signup, Login, Post, PostForm, PostCard, AuthLayout}
